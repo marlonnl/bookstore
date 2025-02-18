47a4c3ebc9216ec7356be3160016994940518144
Marlonn Locatelli <marlonn@gmail.com>
2025-02-18 18:13:54 -0300

    feat: tests para serializers e model

---
+++ product/serializers/category_serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from product.models.category import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Category
+		field = [
+			'title',
+			'slug',
+			'description',
+			'active',
+		]
+		fields = "__all__"
