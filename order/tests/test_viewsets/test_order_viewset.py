df01aba7c8949715c89e1cd046b49a0e01ae2944
Marlonn Locatelli <marlonn@gmail.com>
2025-05-16 15:41:59 -0300

    fix: removed auth from test viewset

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
 # auth
-from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.models import Token
 
 from django.urls import reverse
 
@@ -15,55 +15,61 @@ from order.models import Order
 
 
 class TestOrderView(APITestCase):
-	client = APIClient()
+    client = APIClient()
 
-	def setUp(self):
-		self.category = CategoryFactory(title="technology")
-		self.product = ProductFactory(title="mouse", price=100, category=[self.category])
-		self.order = OrderFactory(product=[self.product])
+    def setUp(self):
+        self.category = CategoryFactory(title="technology")
+        self.product = ProductFactory(
+            title="mouse", price=100, category=[self.category]
+        )
+        self.order = OrderFactory(product=[self.product])
 
-		# auth
-		self.user = UserFactory()
-		token = Token.objects.create(user=self.user)
-		token.save()
+        # auth
+        # self.user = UserFactory()
+        # token = Token.objects.create(user=self.user)
+        # token.save()
 
-	def test_order(self):
-		# auth
-		token = Token.objects.get(user__username=self.user.username)
-		self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    def test_order(self):
+        # auth
+        # token = Token.objects.get(user__username=self.user.username)
+        # self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
-		response = self.client.get(
-			reverse("order-list", kwargs={ "version": "v1" })
-		)
+        response = self.client.get(reverse("order-list", kwargs={"version": "v1"}))
 
-		self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-		order_data = json.loads(response.content)
-		self.assertEqual(order_data["results"][0]["product"][0]["title"], self.product.title)
-		self.assertEqual(order_data["results"][0]["product"][0]["price"], self.product.price)
-		self.assertEqual(order_data["results"][0]["product"][0]["active"], self.product.active)
-		self.assertEqual(order_data["results"][0]["product"][0]["category"][0]["title"], self.category.title)
+        order_data = json.loads(response.content)
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["title"], self.product.title
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["price"], self.product.price
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["active"], self.product.active
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["category"][0]["title"],
+            self.category.title,
+        )
 
-	def test_create_order(self):
-		# user = UserFactory()
-		product = ProductFactory()
-		data = json.dumps({
-			"products_id": [product.id],
-			"user": self.user.id
-		}) 	           
+    def test_create_order(self):
+        # user = UserFactory()
+        product = ProductFactory()
+        data = json.dumps({"products_id": [product.id], "user": self.user.id})
 
-		# auth
-		token = Token.objects.get(user__username=self.user.username)
-		self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)               
+        # auth
+        # token = Token.objects.get(user__username=self.user.username)
+        # self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
-		response = self.client.post(
-			reverse("order-list", kwargs={ "version": "v1" }),
-			data=data,
-			content_type="application/json"
-		)
+        response = self.client.post(
+            reverse("order-list", kwargs={"version": "v1"}),
+            data=data,
+            content_type="application/json",
+        )
 
-		# import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
 
-		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-		
-		created_order = Order.objects.get(user=self.user)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        created_order = Order.objects.get(user=self.user)
